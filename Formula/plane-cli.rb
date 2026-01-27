545959e6e6ad267e6454afa78bf86876680f4174
Radja Thaher <radjapolem@gmail.com>
2026-01-28 01:15:00 +0700

    chore(brew): update sha256

--- Formula/plane-cli.rb
+++ Formula/plane-cli.rb
@@ -7,7 +7,7 @@ class PlaneCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/radjathaher/plane-cli/releases/download/v0.1.0/plane-cli-0.1.0-darwin-aarch64.tar.gz"
-      sha256 "e6853b14e626e2ea3910838c3f1b7755fe7595762d7d86111283b35eccee25cf"
+      sha256 "cb09dbe73cf67fa5bf1ce1f208fe8cdb22cee88c8bc3d67840f19260a037fdd4"
     else
       odie "plane-cli is only packaged for macOS arm64"
     end
